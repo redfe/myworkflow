6d82c51319603280a708633ffb27f322752b2c78
redfe <tetsu999@gmail.com>
2025-05-07 13:09:26 +0900

    refactor: usersのPromiseを使用してローディング状態を追加した

--- src/routes/demo/db/+page.svelte
+++ src/routes/demo/db/+page.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 	let { data } = $props();
-	let users = data.users;
+	let usersPromise = data.users;
 </script>
 
-<pre class="overflow-x-auto">{JSON.stringify(users, null, 2)}</pre>
+{#await usersPromise}
+	<p class="p-4">Loading...</p>
+	<div class="space-y-4">
+		<div class="placeholder animate-pulse"></div>
+		<div class="grid grid-cols-4 gap-4">
+			<div class="placeholder animate-pulse"></div>
+			<div class="placeholder animate-pulse"></div>
+			<div class="placeholder animate-pulse"></div>
+			<div class="placeholder animate-pulse"></div>
+		</div>
+		<div class="placeholder animate-pulse"></div>
+		<div class="placeholder animate-pulse"></div>
+	</div>
+{:then users}
+	<pre class="overflow-x-auto">{JSON.stringify(users, null, 2)}</pre>
+{/await}
