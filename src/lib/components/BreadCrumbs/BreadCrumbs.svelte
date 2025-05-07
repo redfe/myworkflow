c7164b560b81b1bcba711fd42034be45f1fc4bde
redfe <tetsu999@gmail.com>
2025-05-07 13:08:51 +0900

    refactor: 不要な条件分岐を削除してコードを簡素化した

--- src/lib/components/BreadCrumbs/BreadCrumbs.svelte
+++ src/lib/components/BreadCrumbs/BreadCrumbs.svelte
@@ -9,11 +9,7 @@
 	<ol class="mb-6 hidden items-center gap-4 md:flex">
 		{#each items as item, i}
 			<li class="flex items-center gap-2">
-				{#if i < items.length - 1}
-					<a class="opacity-60 hover:underline" href={localizeUrl(item.href).href}>{item.label}</a>
-				{:else}
-					<span class="opacity-60">{item.label}</span>
-				{/if}
+				<a class="opacity-60 hover:underline" href={localizeUrl(item.href).href}>{item.label}</a>
 			</li>
 			{#if i < items.length - 1}
 				<li class="opacity-50" aria-hidden="true">&rsaquo;</li>
