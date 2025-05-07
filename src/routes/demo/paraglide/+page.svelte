d95013615bb32b89647bfb32972b1ae6e20b0d07
redfe <tetsu999@gmail.com>
2025-05-07 21:52:58 +0900

    feat: 不要なi18n拡張の説明を削除し、コードをクリーンアップしました

--- src/routes/demo/paraglide/+page.svelte
+++ src/routes/demo/paraglide/+page.svelte
@@ -8,10 +8,3 @@
 	<button class="btn preset-filled-primary-500" onclick={() => setLocale('ja')}>ja</button>
 	<button class="btn preset-filled-primary-500" onclick={() => setLocale('en')}>en</button>
 </div>
-<p>
-	If you use VSCode, install the <a
-		class="anchor"
-		href="https://marketplace.visualstudio.com/items?itemName=inlang.vs-code-extension"
-		target="_blank">Sherlock i18n extension</a
-	> for a better i18n experience.
-</p>
