d0eef45085ff7f3444090164ea8a78af4d370962
redfe <tetsu999@gmail.com>
2025-05-11 17:07:50 +0900

    fix: テストでの要素取得方法を修正しました

--- e2e/demo.test.ts
+++ e2e/demo.test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('home page has expected ホーム', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByText('ホーム', { exact: false })).toBeVisible();
+	await expect(page.getByText('ホーム', { exact: false }).first()).toBeVisible();
 });
