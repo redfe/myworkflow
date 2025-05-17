1595b775bac9548d530db5f3042c60a2d042d405
redfe <tetsu999@gmail.com>
2025-05-17 22:14:11 +0900

    feat: 不要なコメントを削除し、レイアウトロード関数の可読性を向上させました

--- src/routes/workflow/+layout.server.ts
+++ src/routes/workflow/+layout.server.ts
@@ -1,10 +1,6 @@
 import type { LayoutServerLoad } from './$types';
 import { m } from '$lib/paraglide/messages';
 
-/**
- * ワークフローページのレイアウトロード関数
- * ワークフローページのタイトルとパンくずリストを設定します
- */
 export const load: LayoutServerLoad = () => {
 	return {
 		title: m.page_title_workflow(),
