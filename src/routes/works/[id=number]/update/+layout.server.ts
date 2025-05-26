39fb0eca4ec5b150bdb5769c7584ae938fc4df89
redfe <tetsu999@gmail.com>
2025-05-26 22:05:52 +0900

    feat: 英語および日本語のメッセージファイルに「作業編集」タイトルを追加し、関連するタイトルを更新しました

--- src/routes/works/[id=number]/update/+layout.server.ts
+++ src/routes/works/[id=number]/update/+layout.server.ts
@@ -3,10 +3,10 @@ import { m } from '$lib/paraglide/messages';
 
 export const load: LayoutServerLoad = () => {
 	return {
-		title: m.page_title_add_work(),
+		title: m.page_title_update_work(),
 		breadCrumbs: [
 			{ label: m.page_title_works(), href: '/works' },
-			{ label: m.page_title_add_work() }
+			{ label: m.page_title_update_work() }
 		]
 	};
 };
