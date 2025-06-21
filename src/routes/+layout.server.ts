3926b95a882ec146d81acf26b65e9088bdaafd7a
redfe <tetsu999@gmail.com>
2025-06-21 14:05:50 +0900

    debug

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -6,9 +6,11 @@ import { m } from '$lib/paraglide/messages';
  * ページのタイトルとパンくずリストの初期値を設定します
  */
 export const load: LayoutServerLoad = (event) => {
+	const headers = event.request.headers;
 	return {
 		title: m.page_title_home(),
 		breadCrumbs: [],
-		userId: event.locals.userId
+		userId: event.locals.userId,
+		headers: Array.from(headers.keys())
 	};
 };
